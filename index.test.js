936c417f2c857c6f591fb5007e0f3b95e98bed77
sangramjit dutta <sangramjitdutta@gmail.com>
2025-09-15 15:37:45 +0530

    feat: tests to check if function can handle any amount of numbers

--- index.test.js
+++ index.test.js
@@ -12,4 +12,10 @@ describe("String Calculator", () => {
   test("should return the sum of two numbers", () => {
     expect(add("1,5")).toBe(6);
   });
+
+  test("should handle any amount of numbers", () => {
+    expect(
+      add("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,101")
+    ).toBe(401);
+  });
 });
